dbe875bbccc22bd3da0e4b2fae3145a9088d2a5b
Software <cookie_00@naver.com>
2024-01-05 13:37:23 +0900

    V1-HelloWorld

--- src/main/java/org/zerock/Main.java
+++ src/main/java/org/zerock/Main.java
@@ -1,8 +1,10 @@
 package org.zerock;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -19,11 +21,24 @@ public class Main {
         System.out.println(client);
 
         InputStream inputStream = client.getInputStream();
+        OutputStream out = client.getOutputStream();
+
         Scanner clientScanner  = new Scanner(inputStream);
 
         String line = clientScanner.nextLine();
 
         System.out.println(line);
 
+        String msg ="<h1>Hello World</h1>";
+
+        out.write(new String("HTTP/1.1 200 OK\r\n").getBytes());
+        out.write(new String("Cache-Control: private\r\n").getBytes());
+        out.write(new String("Content-Length:"+msg.getBytes("UTF-8").length+"\r\n").getBytes());
+        out.write(new String("Content-Type: text/html;charset=UTF-8\r\n\r\n").getBytes());
+
+        out.write(msg.getBytes(StandardCharsets.UTF_8));
+
     }
 }
+
+
